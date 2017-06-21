2fd36361f2e21e43c3b005cf7e7ee1dd52147c1c
Felipe Buenaño <fbuenano>
2017-06-21 09:16:05 -0400

    update flatten method to extend the prototype
    fix spaces
    
    update readme
    
    modify flatten

--- flatten.js
+++ flatten.js
@@ -1,30 +1,29 @@
 
-var arr = [[1, 3, 5], [4, 7, 3], [[2, 3], 4]];
-// => [1, 3, 5, 4, 7, 3, 2, 3, 4]
+/**
+ * flatten - method that flats an array.
+ *
+ * @return {array}  - a flatten array.
+ */
+exports.flatten = function flatten(arr) {
+  var flat = [];
 
-
-function flatten(arr) {
-    var flat = [];
-
-    // handle array
-    function handleArray(a) {
-      for(var i=0; i<a.length; i++) {
-          var elem = a[i];
-          if(!Array.isArray(elem)) {
-            flat.push(elem);
-          }
-          handleArray(elem);
-
+  // handle array
+  function handleArray(a) {
+    for (var i = 0; i < a.length; i++) {
+      var elem = a[i];
+      if (!Array.isArray(elem)) {
+        flat.push(elem);
       }
+      handleArray(elem);
     }
-    handleArray(arr);
+  }
+  handleArray(arr);
 
-    return flat;
+  return flat;
 }
 
-var flatArray = flatten(arr);
-console.log(flatArray)
-
+var arr = [[1, 3, 5], [4, 7, 3], [[2, 3], 4]];
+// => [1, 3, 5, 4, 7, 3, 2, 3, 4]
 var array2 = [
   [
     1,
@@ -53,4 +52,6 @@ var array2 = [
      4
    ]
  ];
-console.log(flatten(array2));
+
+console.log(exports.flatten(arr));
+console.log(exports.flatten(array2));
